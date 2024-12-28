0de97ed4c831e3282f06c61ecb77c4fb0491d7a3
nitroqwe2010 <nitroasd71@gmail.com>
2024-12-28 09:21:51 -0500

    salom

---
+++ main.js
@@ -0,0 +1,11 @@
+let person = {
+    name: null,
+    id : null,
+    password : null,
+}
+
+person.name = prompt('name  kiriting');
+person.id = prompt('id kiriting');
+person.password = prompt('password kiriting');
+
+console.log(person);
