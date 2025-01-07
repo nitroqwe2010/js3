619cfb7a3b02c34ef7436a05b5719a0be9532e4c
nitroqwe2010 <nitroasd71@gmail.com>
2025-01-07 03:50:33 -0500

    js1

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ let person = {
     name: null,
     id : null,
     password : null,
+    yoqtirgan : null,
+    tuman : null,
 }
 
-person.name = prompt('name  kiriting');
-person.id = prompt('id kiriting');
-person.password = prompt('password kiriting');
-
-console.log(person);
+person.name = prompt('ismigizn kiriting');
+person.id = prompt('familiya kiriting');
+person.password = prompt('yoshingiz kiriting');
+person.yoqtirgan = prompt('yoqtirgan fan kiriting');
+person.tuman = prompt('yashaydigan tumanigizni kiriting');
+alert ("malumod uchun raxmat")
+alert (`sizning ismigizn; ${person.name} \n familiya;  ${person.id} \n sizning yoshingiz; ${person.password} \n yoqtirgan fan; ${person.yoqtirgan} \n tumanigiz; ${person.tuman} `); 
